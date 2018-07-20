2df0a30cf38494862c4dc39ccd7707cd9cb53aab
dhanisthlm <dhanisthlm@gmail.com>
2018-07-20 15:58:27 +0200

    removed unused files

--- client/components/movielist/queries.js
+++ client/components/movielist/queries.js
@@ -1,15 +1,5 @@
 import gql from 'graphql-tag';
 
-export const GET_MOVIE = gql`
-    query movie($id: String!) {
-        movie(id: $id) {
-            id
-            manifest
-            name
-        }
-    }
-`;
-
 export const GET_MOVIES = gql`
     query movies {
         movies {
